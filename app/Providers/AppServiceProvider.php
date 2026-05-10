6c31a9f154f9930a8475f59a22422ebc7609b687
Mariwow <220711664@students.uajy.ac.id>
2026-05-11 02:15:36 +0700

    SEMOGA BISA HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (config('app.env') === 'production' || env('FORCE_HTTPS', true)) {
+            URL::forceRootUrl(config('app.url'));
             URL::forceScheme('https');
         }
     }
